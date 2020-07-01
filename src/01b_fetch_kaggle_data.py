f0c8ba79309f931b6d38edba25c30fb0c9805a5b
Edward Ross <edward@skeptric.com>
2020-07-01 22:03:25 +1000

    Add Australian Kaggle sources

---
+++ src/01b_fetch_kaggle_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from kaggle.api.kaggle_api_extended import KaggleApi
+from pathlib import Path
+
+
+DEST_DIR = Path().absolute().parent / 'data' / '01_raw' / 'kaggle'
+DEST_DIR.mkdir(exist_ok=True, parents=True)
+
+api = KaggleApi()
+
+api.authenticate()
+api.dataset_download_files('PromptCloudHQ/australian-job-listings-data-from-seek-job-board', path=DEST_DIR, quiet=False, unzip=True)
+api.dataset_download_files('PromptCloudHQ/australian-jobs-on-gumtreecomau', path=DEST_DIR, quiet=False, unzip=True)
