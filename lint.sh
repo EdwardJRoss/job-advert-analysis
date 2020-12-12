53630a487bbb7f45063e838388008ae741720f3b
Edward Ross <edward@skeptric.com>
2020-12-12 13:17:27 +1100

    Remove isort from linting; too slow
    Use isort correctly when formatting

--- lint.sh
+++ lint.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 python -m pyflakes src/
 
 # Style
-python -m isort -m 3 --trailing-comma -c src/
-python -m isort -m 3 --trailing-comma -c tests/
 black --check -t py36 src/ tests/
 cd src
 mypy $(find . -name '*.py')
